c8b70b5ffa8e75fae4b3841bead5488db62fe004
Xiaofeng Liu <liuxf@hust.edu.cn>
2022-08-02 15:39:46 +0800

    Delete example config files for kss and jsut datasets; Change the relative paths of test files; Delete copyright comments from test_fastspeech2.py.

--- test/test_fastspeech2.py
+++ test/test_fastspeech2.py
@@ -1,18 +1,3 @@
-# -*- coding: utf-8 -*-
-# Copyright 2020 Minh Nguyen (@dathudeptrai)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import logging
 import os
 import yaml
@@ -47,12 +32,10 @@ def test_fastspeech_resize_positional_embeddings(new_size):
 @pytest.mark.parametrize(
     "var_train_expr, config_path",
     [
-        (None, "./examples/fastspeech2/conf/fastspeech2.v1.yaml"),
-        ("embeddings|encoder", "./examples/fastspeech2/conf/fastspeech2.v1.yaml"),
-        ("embeddings|encoder", "./examples/fastspeech2/conf/fastspeech2.v2.yaml"),
-        ("embeddings|encoder", "./examples/fastspeech2/conf/fastspeech2.baker.v2.yaml"),
-        ("embeddings|encoder", "./examples/fastspeech2/conf/fastspeech2.kss.v1.yaml"),
-        ("embeddings|encoder", "./examples/fastspeech2/conf/fastspeech2.kss.v2.yaml"),
+        (None, "../examples/fastspeech2/conf/fastspeech2.v1.yaml"),
+        ("embeddings|encoder", "../examples/fastspeech2/conf/fastspeech2.v1.yaml"),
+        ("embeddings|encoder", "../examples/fastspeech2/conf/fastspeech2.v2.yaml"),
+        ("embeddings|encoder", "../examples/fastspeech2/conf/fastspeech2.baker.v2.yaml"),
     ],
 )
 def test_fastspeech2_train_some_layers(var_train_expr, config_path):
