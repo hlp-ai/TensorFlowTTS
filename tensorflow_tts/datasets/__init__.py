2ce522eda4eecf761190d06df56b528b3922cf2f
Xiaofeng Liu <liuxf@hust.edu.cn>
2022-10-26 16:46:10 +0800

    Delete unused audio_dataset.py. Make method abstract.

--- tensorflow_tts/datasets/__init__.py
+++ tensorflow_tts/datasets/__init__.py
@@ -1,3 +1,2 @@
 from tensorflow_tts.datasets.abstract_dataset import AbstractDataset
-from tensorflow_tts.datasets.audio_dataset import AudioDataset
 from tensorflow_tts.datasets.mel_dataset import MelDataset
--- tensorflow_tts/datasets/abstract_dataset.py
+++ tensorflow_tts/datasets/abstract_dataset.py
@@ -1,8 +1,5 @@
 """Abstract Dataset modules."""
-
 import abc
-
-import tensorflow as tf
 
 
 class AbstractDataset(metaclass=abc.ABCMeta):
@@ -36,28 +33,4 @@ class AbstractDataset(metaclass=abc.ABCMeta):
         map_fn=None,
         reshuffle_each_iteration=True,
     ):
-        """Create tf.dataset function."""
-        output_types = self.get_output_dtypes()
-        datasets = tf.data.Dataset.from_generator(
-            self.generator, output_types=output_types, args=(self.get_args())
-        )
-
-        if allow_cache:
-            datasets = datasets.cache()
-
-        if is_shuffle:
-            datasets = datasets.shuffle(
-                self.get_len_dataset(),
-                reshuffle_each_iteration=reshuffle_each_iteration,
-            )
-
-        if batch_size > 1 and map_fn is None:
-            raise ValueError("map function must define when batch_size > 1.")
-
-        if map_fn is not None:
-            datasets = datasets.map(map_fn, tf.data.experimental.AUTOTUNE)
-
-        datasets = datasets.batch(batch_size)
-        datasets = datasets.prefetch(tf.data.experimental.AUTOTUNE)
-
-        return datasets
+        pass
